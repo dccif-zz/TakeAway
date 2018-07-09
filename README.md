b4c57437e22826591fe74b75783546a989845938
dccif <heyujieboy@163.com>
2018-07-09 19:05:59 +0800

    init the repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# 外卖订餐系统
+---
+
+包括：
+
+1. foodadmin 后台管理页面
+2. foodservice 后台服务程序
+3. takeawayapk 外卖应用程序
+
---
+++ foodservice/src/main/java/top/dccif/foodservice/FoodserviceApplication.java
@@ -0,0 +1,12 @@
+package top.dccif.foodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodserviceApplication.class, args);
+    }
+}
